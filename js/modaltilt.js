a8fa19940262e7a9d7f23c7ccb6333a5c35b86af
imambungo <imambungo@gmail.com>
2018-06-07 08:25:33 +0700

    as your wish ¯\_(ツ)_/¯

---
+++ js/modaltilt.js
@@ -0,0 +1,14 @@
+function setDefault(a){
+			document.getElementById(a).style.boxShadow = "inset 0 0 0 2px #424446";
+			document.getElementById(a).style.color = "#222426";
+		}
+		window.onload = setTimeout(function () {
+			setDefault("twitter");
+			setDefault("instagram");
+			setDefault("facebook");
+			setDefault("line");
+		}, 4000);
+		function setHover(id, warna){
+			document.getElementById(id).style.boxShadow = "inset 0 0 0 30px #424446";
+			document.getElementById(id).style.color = warna;
+		}
