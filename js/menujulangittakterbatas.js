6d361bf6f4652b8fc44f63bac574949d70de292f
imambungo <imambungo@gmail.com>
2018-06-08 15:19:28 +0700

    shorter transition time

--- js/menujulangittakterbatas.js
+++ js/menujulangittakterbatas.js
@@ -1,14 +1,14 @@
 function menujulangittakterbatas(a) {
 			document.getElementById("html").className = "animated bounceOutDown";
-			document.getElementById("body").style.WebkitTransition = 'background-color 0.4s ease-in';
-			document.getElementById("body").style.MozTransition = 'background-color 0.4s ease-in';
-			document.getElementById("body").style.Transition = 'background-color 0.4s ease-in';
+			document.getElementById("body").style.WebkitTransition = 'background-color 0.2s ease-in';
+			document.getElementById("body").style.MozTransition = 'background-color 0.2s ease-in';
+			document.getElementById("body").style.Transition = 'background-color 0.2s ease-in';
 			setTimeout(function() { document.body.style.backgroundColor = "#90B5C6"; }, 2100);
 			setTimeout(function() {
 				document.getElementById("body").style.WebkitTransition = 'background-color 0.2s ease-in';
 				document.getElementById("body").style.MozTransition = 'background-color 0.2s ease-in';
 				document.getElementById("body").style.Transition = 'background-color 0.2s ease-in';
-			}, 2500);
-			setTimeout(function() { document.body.style.backgroundColor = "#111"; }, 2500);
-			setTimeout(function() { window.location.href = a; }, 2800);
+			}, 2300);
+			setTimeout(function() { document.body.style.backgroundColor = "#111"; }, 2300);
+			setTimeout(function() { window.location.href = a; }, 2450);
 		}
